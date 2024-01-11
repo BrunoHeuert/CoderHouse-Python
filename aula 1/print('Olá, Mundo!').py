618b77d4f39a13f7692b8a0ecdf2abba71e73e61
BrunoHeuert <brunoheuert@hotmail.com>
2024-01-11 20:02:38 -0300

    Salvando alterações

--- aula 1/print('Olá, Mundo!').py
+++
@@ -1,3 +0,0 @@
-print('Hello, World!')
-n = str(input('Testando Input: '))
-print('Texto digitado: {}'.format(n))
--- aula 3/aula 03 Exercícios.py
+++
@@ -1,7 +0,0 @@
-idade = 15
-altura = 1.5
-
-if altura > 1.4 and idade > 12:
-    print('Pode entrar')
-else:
-    print('Não pode entrar')
